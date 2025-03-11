2bac82c9daa2f72263ffb2cdb48e7e1484bd8b4e
martijnlaffort <58423267+martijnlaffort@users.noreply.github.com>
2025-03-11 08:00:13 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace MartijnLaffort\TranslationScanner\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use MartijnLaffort\TranslationScanner\TranslationScannerServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
