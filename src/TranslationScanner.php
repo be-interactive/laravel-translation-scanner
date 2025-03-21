c1550cccef810943ffedfd90b8896cd0625f92b9
Timo de Winter <timo@be-interactive.nl>
2025-03-21 17:03:53 +0100

    Add support for storing records

--- src/TranslationScanner.php
+++ src/TranslationScanner.php
@@ -7,6 +7,7 @@ use BeInteractive\TranslationScanner\Concerns\CanHaveScanners;
 use BeInteractive\TranslationScanner\Contracts\Scanner;
 use BeInteractive\TranslationScanner\Scanners\TranslationFilesScanner;
 use Illuminate\Support\Arr;
+use Spatie\TranslationLoader\LanguageLine;
 
 class TranslationScanner
 {
@@ -32,9 +33,44 @@ class TranslationScanner
         return $languageLines;
     }
 
-    public function store(bool $deleteLinesThatWereNotScanned = true)
+    public function store(bool $deleteLinesThatWereNotScanned = true): array
     {
-        // todo
+        $groupsAndKeys = $this->getLanguageLines();
+
+        $result = [
+            'total_count' => 0,
+        ];
+
+        if ($deleteLinesThatWereNotScanned) {
+            $result['deleted_count'] = LanguageLine::query()
+                ->whereNotIn('group', array_column($groupsAndKeys, 'group'))
+                ->orWhereNotIn('key', array_column($groupsAndKeys, 'key'))
+                ->delete();
+        } else {
+            $result['deleted_count'] = 0;
+        }
+
+        $existingLanguageLines = LanguageLine::whereIn('group', array_column($groupsAndKeys, 'group'))
+            ->orWhereIn('key', array_column($groupsAndKeys, 'key'))
+            ->get();
+
+        foreach ($groupsAndKeys as $groupAndKey) {
+            $existingItem = $existingLanguageLines->where('group', $groupAndKey['group'])
+                ->where('key', $groupAndKey['key'])
+                ->first();
+
+            if (is_null($existingItem)) {
+                LanguageLine::create([
+                    'group' => $groupAndKey['group'],
+                    'key' => $groupAndKey['key'],
+                    'text' => $groupAndKey['text'],
+                ]);
+
+                $result['total_count'] += 1;
+            }
+        }
+
+        return $result;
     }
 
 
